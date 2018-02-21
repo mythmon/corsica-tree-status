0b1293b9602b0e29cb911e1f06d8b2f89b3b3719
Mike Cooper <mythmon@gmail.com>
2018-02-21 09:04:56 -0800

    When in Corsica, trigger a reset if nothing is interesting.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const treeNames = [
+const realTreeNames = [
+  'ash',
   'birch',
   'cedar',
   'cypress',
   'elm',
   'holly',
   'jamun',
+  'larch',
   'maple',
   'oak',
   'pine',
-  'larch',
-  'ash',
 ];
+
+/**
+ * Try and guess if the page is being shown in Corsica currently.
+ */
+function probablyInCorsica() {
+  // Corsica puts pages in iframes. Cross origin iframe children can't
+  // access certain properties of their parents. If this seems to be
+  // the case, assume that we are being loaded by Corsica.
+  try {
+    window.parent.location.toString();
+    // if the above does  not throw, not in an iframe
+  } catch (err) {
+    if (err.name === "SecurityError" && err.message.includes("toString")) {
+      // This is the error we are looking for.
+      return true;
+    }
+  }
+  // This doesn't look like corsica
+  return false;
+}
+
+// If the shown statuses match these expected statuses, it is boring.
+const expectedStatuses = {
+  "ash": "open",
+  "autoland": "open",
+  "birch": "open",
+  "cedar": "open",
+  "comm-aurora-seamonkey": "closed",
+  "comm-aurora-thunderbird": "closed",
+  "comm-beta-seamonkey": "approval required",
+  "comm-beta-thunderbird": "approval required",
+  "comm-central-seamonkey": "open",
+  "comm-central-thunderbird": "open",
+  "comm-esr45-thunderbird": "approval required",
+  "comm-esr52-seamonkey": "approval required",
+  "comm-esr52-thunderbird": "approval required",
+  "comm-release-seamonkey": "approval required",
+  "comm-release-thunderbird": "approval required",
+  "cypress": "open",
+  "elm": "open",
+  "graphics": "open",
+  "holly": "open",
+  "jamun": "open",
+  "larch": "open",
+  "maple": "open",
+  "mozilla-aurora": "closed",
+  "mozilla-beta": "approval required",
+  "mozilla-central": "approval required",
+  "mozilla-esr52": "approval required",
+  "mozilla-inbound": "open",
+  "mozilla-release": "approval required",
+  "nss": "open",
+  "nss-try": "open",
+  "oak": "open",
+  "pine": "open",
+  "try-comm-central": "open",
+  "try": "open",
+};
 
 class App extends Component {
   constructor(props) {
@@ -32,7 +90,7 @@ class App extends Component {
   updateTreeStatues(treeStatuses) {
     treeStatuses = treeStatuses
       .filter(({ tree }) => !tree.includes('comm-'))
-      .filter(({ tree }) => !treeNames.includes(tree))
+      .filter(({ tree }) => !realTreeNames.includes(tree))
       .filter(({ tree }) => tree !== "mozilla-aurora");
 
     function treeScore({ tree }) {
@@ -56,6 +114,24 @@ class App extends Component {
     }
 
     treeStatuses.sort((a, b) => treeScore(b) - treeScore(a));
+
+    if (probablyInCorsica()) {
+      const interesting = treeStatuses.filter(ts => expectedStatuses[ts.tree] !== ts.status);
+      if (interesting.length === 0) {
+        // There isn't anything interesting here, so try and ask
+        // Corsica to advance to the next page. Delay setting the tree
+        // statuses for a moment, to avoid flickering content while
+        // changeing to the next page. Still load the content
+        // eventually, in case we are wrong about being in Corsica, or
+        // the reset message isn't received for some reason.
+        setTimeout(() => this.setState({ treeStatuses }), 1000);
+        window.parent.postMessage({
+          corsica: true,
+          message: 'reset',
+        }, "*");
+        return;
+      }
+    }
 
     this.setState({ treeStatuses });
   }
